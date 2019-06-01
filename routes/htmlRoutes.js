25739b16771499f66b5c4e6715c377304f4d50ee
Ches-CC <chesley.calloway@gmail.com>
2019-06-01 13:57:19 -0700

    continued work on general handlebar & site structure; adjust appropriate hmtlRoutes to accomodate as much; the htmlRoutes ntb updated however as they still have info related to the example's/boilerplate's initial content

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -10,7 +10,48 @@ module.exports = function(app) {
       });
     });
   });
-
+  // Render Other Pages
+  // the actual "db" content ntb edited/updated
+  app.get("/results", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("results", {
+        msg: "Results!",
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/history", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("history", {
+        msg: "Results!",
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/about", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("about", {
+        msg: "Results!",
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/other-resources", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("other-resources", {
+        msg: "Results!",
+        examples: dbExamples
+      });
+    });
+  });
+  app.get("/create-account", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("create-account", {
+        msg: "Results!",
+        examples: dbExamples
+      });
+    });
+  });
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
