ac409c4701c378d09423f68211cd907d9cafdd27
AndrewHanson1 <47340852+AndrewHanson1@users.noreply.github.com>
2019-06-07 04:25:56 -0700

    spotify results

--- models/index.js
+++ models/index.js
@@ -1,15 +1,14 @@
-    
+
 'use strict';
 
 var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV;
+var env       = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
-var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-
 fs
   .readdirSync(__dirname)
   .filter(function (file) {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,64 @@
 var path = require("path");
 
-module.exports = function(app) {
+var Spotify = require("node-spotify-api");
+
+var spotify = new Spotify({
+  id: "cdeb18114adf4c16950cb4dfb8e714c8",
+  secret: "c63db428d5f74e15ad12a61d6b58bf4b"
+});
+
+var resultFunctions = {
+
+  getMeASong: function getMeASong(res) {
+    spotify.search(
+      {
+        type: "playlist",
+        query: "happy",
+        limit: 1
+      },
+      function (err, data) {
+        if (err) {
+          return console.log("Error occurred: " + err);
+        }
+        //console.log(data.playlists.items);
+        console.log(data.playlists.items[0].external_urls.spotify);
+
+        var song = data.playlists.items[0].external_urls.spotify;
+        var firstPart = song.slice(0, 24);
+        var secondPart = song.slice(34, 56)
+        var combine = firstPart + "/embed/playlist/" + secondPart;
+        console.log(combine);
+
+        function sendHbs(combine) {
+          var hbsObject = {
+            combine: combine
+          };
+          console.log(hbsObject);
+          res.render("results", hbsObject);
+          return
+        }
+        sendHbs(combine);
+
+
+
+
+
+      }
+    )
+  }
+
+
+
+
+}
+
+
+
+
+
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index");
   });
   // app.get("/", function(req, res, next) {
@@ -11,25 +67,64 @@ module.exports = function(app) {
 
   // Render Other Pages
   // the actual "db" content ntb edited/updated
-  app.get("/results", function(req, res) {
-    res.render("results");
+  app.get("/results", function (req, res) {
+    /* function getMeASong() {
+      spotify.search(
+        {
+          type: "playlist",
+          query: "happy",
+          limit: 1
+        },
+        function (err, data) {
+          if (err) {
+            return console.log("Error occurred: " + err);
+          }
+          //console.log(data.playlists.items);
+          console.log(data.playlists.items[0].external_urls.spotify);
+
+          var song = data.playlists.items[0].external_urls.spotify;
+          var firstPart = song.slice(0, 24);
+          var secondPart = song.slice(34, 56)
+          var combine = firstPart + "/embed/playlist/" + secondPart;
+          console.log(combine);
+
+          function sendHbs(combine) {
+            var hbsObject = {
+              combine: combine
+            };
+            console.log(hbsObject);
+            res.render("results", hbsObject);
+            return
+          }
+          sendHbs(combine);
+
+
+
+
+
+        }
+      )
+    } */
+
+
+    resultFunctions.getMeASong(res);
   });
-  app.get("/history", function(req, res) {
+  app.get("/history", function (req, res) {
     res.render("history");
   });
-  app.get("/about", function(req, res) {
+  app.get("/about", function (req, res) {
     res.render("about");
   });
-  app.get("/other-resources", function(req, res) {
+  app.get("/other-resources", function (req, res) {
     res.render("other-resources");
   });
-  app.get("/signin", function(req, res) {
+  app.get("/signin", function (req, res) {
     res.render("signin");
   });
-  app.get("/signup", function(req, res) {
+  app.get("/signup", function (req, res) {
     res.render("results");
   });
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("index");
   });
 }
