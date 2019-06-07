aa1b6da1dc729da43ac401dec18590cf1b422844
reiko321 <reiko321@gmail.com>
2019-06-06 19:16:50 -0700

    api routing for DB

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,88 @@
-var db = require("../models");
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
 
-module.exports = function(app) {
+// Dependencies
+// =============================================================
 
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+// Requiring our History model
+var db = require("./models");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+
+  // GET route for getting all of the history for one ID
+  app.get("/api/history/id/:id", function (req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.History
+      .findAll({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function (results) {
+        // We have access to the History as an argument inside of the callback function
+        res.json(results);
+      });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // // GET route for getting all history
+  // app.get("/api/history/", function (req, res) {
+  //   // findAll returns all entries for a table when used with no options
+  //   db.History
+  //     .findAll({})
+  //     .then(function (results) {
+  //       // We have access to the History as an argument inside of the callback function
+  //       res.json(results);
+  //     });
+  // });
+
+  // POST route for saving states initially
+  app.post("/api/confirms", function (req, res) {
+    console.log(req.body);
+    // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just we pass in an object with a text
+    // and complete property (req.body)
+    db.History
+      .create({
+        current_state: req.body.current_state,
+        desired_state: req.body.desired_state
+      }).then(function (results) {
+        // We have access to the new entry as an argument inside of the callback function
+        res.json(results);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+ // PUT route for updating from history page.
+  app.post("/api/history", function (req, res) {
+    console.log(req.body);
+    // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just we pass in an object with a text
+    // and complete property (req.body)
+    db.History
+    .update(req.body, {
+      where: {
+        successful: req.body.successful
+      }
+    })
+    .then(function (results) {
+      res.json(results);
   });
+
+  // DELETE route for deleting entries. We can get the id of the entry we want to delete from
+  // req.params.id
+  app.delete("/api/history/:id", function (req, res) {
+    db.History
+      .destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function (results) {
+        // We have access to the new History as an argument inside of the callback function
+        res.json(results);
+      });
+  });
+
 };
