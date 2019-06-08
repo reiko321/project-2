f16841d24fcd48ca38e6627fa6f198cf868df531
unknown <andrew.lewis.hanson@gmail.com>
2019-06-07 22:28:55 -0700

    https

--- public/js/results.js
+++ public/js/results.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         var titleArray = ["Little Miss Sunshine", "Forrest Gump", "Love Actually", "Depspicable Me", "The Sound of Music", "It's a Wonderful Life", "9 to 5: Inside Out"];
         var decider = Math.floor(Math.random() * 6);
         var title = titleArray[decider];
-        var firstPart = "http://www.omdbapi.com/?t=";
+        var firstPart = "https://www.omdbapi.com/?t=";
         var secondPart = "&y=&plot=short&apikey=cd5d920a";
         var combine = firstPart + title + secondPart;
 
